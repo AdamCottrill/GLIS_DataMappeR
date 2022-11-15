fb6ba24790a2f7fb0bfb1db4687cfd4e2b4c2a62
Rachel Henderson <rhenderson1993@gmail.com>
2022-11-15 15:29:15 -0500

    Updates to reflect changes to template 4

--- create_prj_db.R
+++ create_prj_db.R
@@ -8,124 +8,164 @@ source("./data_mapper_utils.R")
 
 # Select the project code, process type, protocol, and lake (enter all as strings)
 # Refer to the data dictionary for a list of process types and their definitions
-prj_cd <- "LHA_IA14_801"
+prj_cd <- 'LHA_IA15_808'
 process_type <- "2"
 protocol <- "BSM"
 lake <- "HU"
-lamsam <- "1"
+lamsam <- "2"
 overwrite <- TRUE
 
 # source database
-src_dbase <- "../../data_modernization/fn_portal_data_in/NearshoreMapper_template3.accdb"
-
-# src_dbase <-"C:/Users/COTTRILLAD/1work/Python/djcode/apps/fn_portal/utils/data_upload_src/OffshoreMapper.accdb"
+#src_dbase <- "./HuronOffshoreMapper.accdb"
+src_dbase <- "./NearshoreMapper_template4.accdb"
+#src_dbase <- "./SmallfishMapper.accdb"
 
 # template database:
-template_db <- "../../data_modernization/fn_portal_data_in/templates/Great_Lakes_Template3_template.accdb"
+template_db <- "./templates/Great_Lakes_Template_4.accdb"
 
-trg_db <- file.path(dirname(src_dbase), "build", paste0(prj_cd, ".accdb"))
+trg_db <- file.path(dirname(src_dbase),'build', paste0(prj_cd, '.accdb'))
 
-file.copy(template_db, trg_db, overwrite = overwrite)
+file.copy(template_db, trg_db, overwrite=overwrite)
 
 
-fn011 <- fetch_data("FN011", prj_cd, src_dbase)
+fn011 <- fetch_data('FN011', prj_cd, src_dbase)
 head(fn011)
 fn011$PRJ_DATE0 <- as.Date(fn011$PRJ_DATE0)
 fn011$PRJ_DATE1 <- as.Date(fn011$PRJ_DATE1)
-append_data(trg_db, "FN011", fn011)
+append_data(trg_db, 'FN011', fn011)
 
 
-fn012 <- fetch_data("FN012", prj_cd, src_dbase)
+fn012 <- fetch_data('FN012', prj_cd, src_dbase)
 head(fn012)
 names(fn012) <- toupper(names(fn012))
-fn012_default <- get_FN012_protocol(list(lake = lake))
-fn012_default <- unique(subset(fn012_default, select = c(-BIOSAM, -PROTOCOL)))
+fn012_default <- get_FN012_protocol(list(lake = lake, protocol = protocol))
+fn012_default$FLEN_MIN[is.na(fn012_default$FLEN_MIN)] <- 1
+fn012_default$FLEN_MAX[is.na(fn012_default$FLEN_MAX)] <- 1999
+fn012_default$TLEN_MIN[is.na(fn012_default$TLEN_MIN)] <- 1
+fn012_default$TLEN_MAX[is.na(fn012_default$TLEN_MAX)] <- 1999
+fn012_default$RWT_MIN[is.na(fn012_default$RWT_MIN)] <- 0.1
+fn012_default$RWT_MAX[is.na(fn012_default$RWT_MAX)] <- 10000
+fn012_default$K_MIN_ERROR[is.na(fn012_default$K_MIN_ERROR)] <- 0.05
+fn012_default$K_MAX_ERROR[is.na(fn012_default$K_MAX_ERROR)] <- 0.05
+fn012_default$K_MIN_WARN[is.na(fn012_default$K_MIN_WARN)] <- 3.9
+fn012_default$K_MAX_WARN[is.na(fn012_default$K_MAX_WARN)] <- 3.9
+           
+#fn012_default <- unique(subset(fn012_default, select = c(-BIOSAM, -PROTOCOL)))
+fn012_default <- subset(fn012_default, select = c(-BIOSAM))
 fn012 <- merge(fn012, fn012_default, by = c("SPC", "GRP"), all.x = TRUE)
-fn012 <- subset(fn012, select = c(-LAKE))
+fn012 <- subset(fn012, select= c(-LAKE, -PROTOCOL, -AGEDEC))
+fn012$GRP_DES[is.na(fn012$GRP_DES)] <- "default group"
+fn012$SIZSAM[is.na(fn012$SIZSAM)] <- "0"
+fn012$SIZATT[is.na(fn012$SIZATT)] <- "FLEN"
+fn012$SIZINT[is.na(fn012$SIZINT)] <- 1
+fn012$FDSAM[is.na(fn012$FDSAM)] <- 0
+fn012$SPCMRK[is.na(fn012$SPCMRK)] <- "0"
+fn012$AGEST[is.na(fn012$AGEST)] <- "0"
+fn012$FLEN_MIN[is.na(fn012$FLEN_MIN)] <- 1
+fn012$FLEN_MAX[is.na(fn012$FLEN_MAX)] <- 1999
+fn012$TLEN_MIN[is.na(fn012$TLEN_MIN)] <- 1
+fn012$TLEN_MAX[is.na(fn012$TLEN_MAX)] <- 1999
+fn012$RWT_MIN[is.na(fn012$RWT_MIN)] <- 1
+fn012$RWT_MAX[is.na(fn012$RWT_MAX)] <- 10000
+fn012$K_MIN_ERROR[is.na(fn012$K_MIN_ERROR)] <- 0.05
+fn012$K_MIN_WARN[is.na(fn012$K_MIN_WARN)] <- 3.9
+fn012$K_MAX_ERROR[is.na(fn012$K_MAX_ERROR)] <- 0.05
+fn012$K_MAX_WARN[is.na(fn012$K_MAX_WARN)] <- 3.9
 fn012$LAMSAM <- lamsam
-append_data(trg_db, "FN012", fn012)
+append_data(trg_db, 'FN012', fn012)
 
 
-fn022 <- fetch_data("FN022", prj_cd, src_dbase)
+fn022 <- fetch_data('FN022', prj_cd, src_dbase)
 head(fn022)
 fn022$SSN_DATE0 <- as.Date(fn022$SSN_DATE0)
 fn022$SSN_DATE1 <- as.Date(fn022$SSN_DATE1)
-append_data(trg_db, "FN022", fn022)
+append_data(trg_db, 'FN022', fn022)
 
 
-fn026 <- fetch_data("FN026", prj_cd, src_dbase)
+fn026 <- fetch_data('FN026', prj_cd, src_dbase)
 head(fn026)
-append_data(trg_db, "FN026", fn026)
+append_data(trg_db, 'FN026', fn026)
+
+fn026_subspace <- fetch_data('FN026_Subspace', prj_cd, src_dbase)
+fn026_subspace$SPACE <- "00"
+fn026_subspace$SUBSPACE <- "00"
+fn026_subspace$SUBSPACE_DES <- "Whole sampling area"
+fn026_subspace <- merge(fn026_subspace, fn026)
+fn026_subspace <- subset(fn026_subspace, select = -c(SPACE_DES, SPACE_WT))
+head(fn026_subspace)
+append_data(trg_db, "FN026_Subspace", fn026_subspace)
 
 
-fn028 <- fetch_data("FN028", prj_cd, src_dbase)
+fn028 <- fetch_data('FN028', prj_cd, src_dbase)
 head(fn028)
 fn028$EFFTM0_GE <- get_time(fn028$EFFTM0_GE)
 fn028$EFFTM0_LT <- get_time(fn028$EFFTM0_LT)
-# increment mode here:
+#increment mode here:
 fn028$MODE <- paste0(seq(1, nrow(fn028)), 0)
-append_data(trg_db, "FN028", fn028)
+append_data(trg_db, 'FN028', fn028)
 
 
 # Get list of gear/effort/process types from the glfishr package (requires VPN connection)
 gear_effort_process_types <- get_gear_process_types()
-gear_effort_process_types <- subset(gear_effort_process_types, GR %in% fn028$GR & PROCESS_TYPE == process_type)
+gear_effort_process_types <- subset(gear_effort_process_types, GR %in% fn028$GR & PROCESS_TYPE %in% process_type)
 
-append_data(trg_db, "Gear_Effort_Process_Types", gear_effort_process_types)
+# Filter out unnecessary combos
+#gear_effort_process_types <- subset(gear_effort_process_types, GR != "SIN1" | PROCESS_TYPE != "1")
+
+append_data(trg_db, 'Gear_Effort_Process_Types', gear_effort_process_types)
 
 
 fn121 <- fetch_data("FN121", prj_cd, src_dbase)
 fn121 <- add_mode(fn121, fn028)
 fn121$SSN <- "00"
-fn121$SPACE <- "00"
-fn121$PROCESS_TYPE <- process_type
+fn121 <- merge(fn121, unique(subset(gear_effort_process_types, select = -c(EFFDST, EFF))))
 fn121$EFFDT0 <- as.Date(fn121$EFFDT0)
 fn121$EFFDT1 <- as.Date(fn121$EFFDT1)
 fn121$EFFTM0 <- get_time(fn121$EFFTM0)
 fn121$EFFTM1 <- get_time(fn121$EFFTM1)
-
-fn121 <- subset(fn121, select = -c(GR, GRUSE, ORIENT))
+fn121$SUBSPACE <- "00"
+fn121 <- subset(fn121, select=-c(GR, GRUSE, ORIENT))
 head(fn121)
-append_data(trg_db, "FN121", fn121)
+append_data(trg_db, 'FN121', fn121)
 
 
-fn122 <- fetch_data("FN122", prj_cd, src_dbase)
+
+fn122 <- fetch_data('FN122', prj_cd, src_dbase)
 head(fn122)
-append_data(trg_db, "FN122", fn122)
+append_data(trg_db, 'FN122', fn122)
 
 
-fn123 <- fetch_data("FN123", prj_cd, src_dbase)
+fn123 <- fetch_data('FN123', prj_cd, src_dbase)
 head(fn123)
-append_data(trg_db, "FN123", fn123)
+append_data(trg_db, 'FN123', fn123)
 update_FN122_waterhaul(trg_db)
+update_FN012_sizsam(trg_db)
 
 
-fn125 <- fetch_data("FN125", prj_cd, src_dbase)
+fn125 <- fetch_data('FN125', prj_cd, src_dbase)
 head(fn125)
-append_data(trg_db, "FN125", fn125)
+append_data(trg_db, 'FN125', fn125)
 
 
 fn125_tags <- fetch_data("FN125_tags", prj_cd, src_dbase)
-# we might need to increment FISH_TAGID here
+#we might need to increment FISH_TAGID here
 head(fn125_tags)
-append_data(trg_db, "FN125_tags", fn125_tags)
-update_FN125_tag_flag(trg_db)
+append_data(trg_db, 'FN125_tags', fn125_tags)
 
 
-fn125_lamprey <- fetch_data("FN125_lamprey", prj_cd, src_dbase)
+fn125_lamprey <- fetch_data('FN125_lamprey', prj_cd, src_dbase)
 fn125_lamprey <- process_fn125_lamprey(fn125_lamprey)
 head(fn125_lamprey)
-append_data(trg_db, "FN125_lamprey", fn125_lamprey)
-update_FN125_lam_flag(trg_db)
+append_data(trg_db, 'FN125_lamprey', fn125_lamprey)
 
 
-fn126 <- fetch_data("FN126", prj_cd, src_dbase)
+fn126 <- fetch_data('FN126', prj_cd, src_dbase)
 head(fn126)
-append_data(trg_db, "FN126", fn126)
+append_data(trg_db, 'FN126', fn126)
 update_FN125_stom_flag(trg_db)
 
 
-fn127 <- fetch_data("FN127", prj_cd, src_dbase)
+fn127 <- fetch_data('FN127', prj_cd, src_dbase)
 head(fn127)
-append_data(trg_db, "FN127", fn127)
-update_FN125_age_flag(trg_db)
+append_data(trg_db, 'FN127', fn127)
+
--- data_mapper_utils.R
+++ data_mapper_utils.R
@@ -1,109 +1,108 @@
 # function to get queries from Access mapper database
 
-get_trg_table_names <- function(trg_db, table) {
+get_trg_table_names <- function(trg_db, table){
   DBConnection <- odbcConnectAccess2007(trg_db, uid = "", pwd = "")
   stmt <- sprintf("select * from [%s] where FALSE;", table)
-  dat <- sqlQuery(DBConnection, stmt, as.is = TRUE, stringsAsFactors = FALSE, na.strings = "")
+  dat <- sqlQuery(DBConnection, stmt, as.is=TRUE, stringsAsFactors=FALSE, na.strings = "")
   odbcClose(DBConnection)
   return(toupper(names(dat)))
 }
 
-# checks column names for differences between the source and target and lists the missing/extra column(s) in
-# the console
-check_table_names <- function(trg_db, table, src_data) {
+check_table_names <- function(trg_db, table, src_data){
   trg_names <- get_trg_table_names(trg_db, table)
   missing <- setdiff(trg_names, names(src_data))
   extra <- setdiff(names(src_data), trg_names)
-  if (length(extra)) {
-    msg <- sprintf(
-      "The source data frame has extra fields: %s",
-      paste(extra, collapse = ", ")
-    )
+  if(length(extra)) {
+    msg <- sprintf("The source data frame has extra fields: %s",
+                   paste(extra,collapse = ', '))
     warning(msg)
   }
-  if (length(missing)) {
-    msg <- sprintf(
-      "The source data frame is missing fields: %s",
-      paste(missing, collapse = ", ")
-    )
+  
+  if(length(missing)) {
+    msg <- sprintf("The source data frame is missing fields: %s",
+                   paste(missing, collapse = ', '))
     warning(msg)
   }
+  
   return(c(extra, missing))
+  
 }
 
 
-# fetches data from Access based on the query name
-fetch_data <- function(table, prj_cd, src_db, toupper = T) {
-  DBConnection <- odbcConnectAccess2007(src_db, uid = "", pwd = "")
+fetch_data <- function(table, prj_cd, src_db){
+  DBConnection <- odbcConnectAccess2007(src_db,uid = "", pwd = "")
   stmt <- sprintf("EXEC get_%s @prj_cd='%s'", table, prj_cd)
-  dat <- sqlQuery(DBConnection, stmt, as.is = TRUE, stringsAsFactors = FALSE, na.strings = "")
+  dat <- sqlQuery(DBConnection, stmt, as.is=TRUE, stringsAsFactors=FALSE, na.strings = "")
   odbcClose(DBConnection)
-
-  if (toupper) names(dat) <- toupper(names(dat))
   return(dat)
 }
 
 
-get_time <- function(datetime) {
+get_time <- function(datetime){
   # extract time from date/time string
   time_regex <- ".*([0-2][0-9]:[0-5][0-9]:[0-5][0-9])$"
   datetime <- gsub(time_regex, "\\1", datetime, perl = TRUE)
   msg <- "One or more values is not a valid time or datetime. The time component should be in the format HH:MM:SS."
-
+  
   ifelse(!is.na(datetime) & !grepl(time_regex, datetime),
     stop(msg),
-    datetime
-  )
+    datetime)
+
 }
 
 
 # function to append data to the template
-append_data <- function(dbase, trg_table, data, append = T, safer = T,
-                        toupper = T, verbose = F) {
-  if (toupper) names(data) <- toupper(names(data))
-
+append_data <- function(dbase, trg_table, data, append=T, safer=T,
+                        toupper=T, verbose=F){
+  if (toupper)names(data) <- toupper(names(data))
+  
   field_check <- check_table_names(dbase, trg_table, data)
-  if (length(field_check)) stop("Please fix field differences before proceeding.")
-
+  if(length(field_check)) stop("Please fix field differences before proceeding.")
+  
   conn <- odbcConnectAccess2007(dbase, uid = "", pwd = "")
-  sqlSave(conn, data,
-    tablename = trg_table, rownames = F,
-    safer = safer, append = append, nastring = NULL, verbose = verbose
-  )
+  sqlSave(conn, data, tablename = trg_table, rownames = F,
+          safer = safer, append=append, nastring = NULL, verbose = verbose)
   return(odbcClose(conn))
+  
+}
+
+add_mode <- function(fn121, fn028){
+  # populate the correct mode for each sam:
+  x121 <- subset(fn121, select=c("PRJ_CD", "SAM", "GR", "GRUSE", "ORIENT"))
+  x028 <- subset(fn028, select=c("PRJ_CD",  "GR", "GRUSE", "ORIENT", "MODE"))
+  tmp <- merge(x121, x028, by=c("PRJ_CD","GR", "GRUSE", "ORIENT"))
+  foo <- merge(fn121, tmp, by=c("PRJ_CD", "SAM", "GR", "GRUSE",
+                                "ORIENT"))
+  
 }
 
 
-
-# helper function to split lamprey wound strings into individual wounds and the wound and diameter components
-split_lamijc <- function(wound) {
+split_lamijc <- function(wound){
   wnd_regex <- "^0$|^([A|B][1-4])+$|^([A|B][1-4][1-5][0-9])+$"
-  if (!grepl(wnd_regex, wound)) {
+  if (!grepl(wnd_regex, wound)){
     msg <- sprintf("'%s' is not a valid lamijc.", wound)
     stop(msg)
   }
 
-  if (wound == "0") {
-    # retrun a matrix to match other output types:
+  if (wound=="0"){
+    #retrun a matrix to match other output types:
     return(matrix(c("0", "")))
   }
 
   a123_regex <- "^([A|B][1-4][1-5][0-9])+$"
-  if (grepl(a123_regex, wound)) {
-    # wound with diameter
+  if (grepl(a123_regex,wound)){
+    #wound with diameter
     wounds <- strsplit(wound, "(?<=.{4})", perl = TRUE)[[1]]
   } else {
     wounds <- strsplit(wound, "(?<=.{2})", perl = TRUE)[[1]]
   }
 
-  # split the wounds up in to wound and diameter components:
-  wounds <- t(sapply(wounds, function(x) c(substr(x, 1, 2), substr(x, 3, 4))))
+  #split the wounds up in to wound and diameter components:
+  wounds <- t(sapply(wounds, function(x) c(substr(x,1,2), substr(x,3,4))))
   return(wounds)
 }
 
 
-
-# adds one row per wound to the lamprey wound table
 process_fn125_lamprey_lamijc <- function(df) {
   tmp <- df[FALSE, ]
   tmp$LAMICJ_TYPE <- character()
@@ -112,43 +111,48 @@ process_fn125_lamprey_lamijc <- function(df) {
   for (i in 1:nrow(df)) {
     fish <- df[i, ]
     wounds <- split_lamijc(df$LAMIJC[i])
-    if (wounds[1, 1] == "0") {
+    if(wounds[1,1]=='0'){
       fish$LAMIJC_TYPE <- "0"
       fish$LAMIJC_SIZE <- NA
       tmp <- rbind(tmp, fish)
     } else {
-      for (w in 1:nrow(wounds)) {
-        fish$LAMID <- w
-        fish$LAMIJC_TYPE <- wounds[w, 1]
-        fish$LAMIJC_SIZE <- wounds[w, 2]
-        tmp <- rbind(tmp, fish)
-      }
+    for (w in 1:nrow(wounds)) {
+      fish$LAMID <- w
+      fish$LAMIJC_TYPE <- wounds[w, 1]
+      fish$LAMIJC_SIZE <- wounds[w, 2]
+      tmp <- rbind(tmp, fish)
     }
+    }
+
+
+
   }
 
   tmp$LAMIJC <- NULL
   return(tmp)
 }
 
-# Checks for null values in LAMIJC and if all values are null (i.e. XLAM was used), returns empty columns for
-# LAMIJC_TYPE and LAMIJC_SIZE
-process_fn125_lamprey <- function(df) {
+process_fn125_lamprey <- function(df){
+  
   sub_df <- subset(df, select = "LAMIJC")
-
-  w <- sapply(sub_df, function(sub_df) all(is.na(sub_df)))
+  
+  w <- sapply(sub_df, function(sub_df)all(is.na(sub_df)))
   if (any(w)) {
     df$LAMIJC_TYPE <- NA
     df$LAMIJC_SIZE <- NA
     df <- subset(df, select = -c(LAMIJC))
-
+    
     return(df)
-  } else {
+    }
+  
+  else {
     process_fn125_lamprey_lamijc(df)
   }
+  
+  
 }
 
-# sets FN122.WATERHAUL values to TRUE or 0 based on values in the FN123 table
-update_FN122_waterhaul <- function(dbase) {
+update_FN122_waterhaul <- function(dbase){
   sql <- "UPDATE FN122 LEFT JOIN FN123
 ON (FN122.EFF = FN123.EFF)
 AND(FN122.SAM = FN123.SAM)
@@ -158,11 +162,25 @@ WHERE (((FN123.PRJ_CD) Is Null));"
   conn <- odbcConnectAccess2007(dbase, uid = "", pwd = "")
   sqlQuery(conn, sql)
   return(odbcClose(conn))
+
 }
 
-# series of functions to update stomach, age, lamprey, and tag flags in FN125 based on presence/absence of records in
-# subsequent tables
-update_FN125_stom_flag <- function(dbase) {
+update_FN012_sizsam <- function(dbase){
+  sql <- "UPDATE FN012 LEFT JOIN FN123
+ON (FN012.SPC = FN123.SPC)
+AND(FN012.GRP = FN123.GRP)
+AND(FN012.PRJ_CD = FN123.PRJ_CD)
+SET FN012.SIZSAM = 1
+WHERE (((FN123.PRJ_CD) Is NOT Null));"
+  conn <- odbcConnectAccess2007(dbase, uid = "", pwd = "")
+  sqlQuery(conn, sql)
+  return(odbcClose(conn))
+  
+}
+
+
+update_FN125_stom_flag <- function(dbase){
+
   sql <- "UPDATE FN125 LEFT JOIN FN126
 ON
 FN126.FISH = FN125.FISH
@@ -171,76 +189,12 @@ AND FN126.GRP = FN125.GRP
 AND FN126.EFF = FN125.EFF
 AND FN126.SAM = FN125.SAM
 AND FN126.PRJ_CD = FN125.PRJ_CD
-SET FN125.STOM_FLAG = 'True'
+SET FN125.STOM_FLAG = '2'
 WHERE FN126.PRJ_CD Is NOT Null;"
   conn <- odbcConnectAccess2007(dbase, uid = "", pwd = "")
   sqlQuery(conn, sql)
 
   return(odbcClose(conn))
+
 }
 
-
-update_FN125_age_flag <- function(dbase) {
-  sql <- "UPDATE FN125 LEFT JOIN FN127
-ON
-FN127.FISH = FN125.FISH
-AND FN127.SPC = FN125.SPC
-AND FN127.GRP = FN125.GRP
-AND FN127.EFF = FN125.EFF
-AND FN127.SAM = FN125.SAM
-AND FN127.PRJ_CD = FN125.PRJ_CD
-SET FN125.AGE_FLAG = 'True'
-WHERE FN127.PRJ_CD Is NOT Null;"
-  conn <- odbcConnectAccess2007(dbase, uid = "", pwd = "")
-  sqlQuery(conn, sql)
-
-  return(odbcClose(conn))
-}
-
-
-update_FN125_lam_flag <- function(dbase) {
-  sql <- "UPDATE FN125 LEFT JOIN FN125_Lamprey
-ON
-FN125_Lamprey.FISH = FN125.FISH
-AND FN125_Lamprey.SPC = FN125.SPC
-AND FN125_Lamprey.GRP = FN125.GRP
-AND FN125_Lamprey.EFF = FN125.EFF
-AND FN125_Lamprey.SAM = FN125.SAM
-AND FN125_Lamprey.PRJ_CD = FN125.PRJ_CD
-SET FN125.LAM_FLAG = 'True'
-WHERE FN125_Lamprey.PRJ_CD Is NOT Null;"
-  conn <- odbcConnectAccess2007(dbase, uid = "", pwd = "")
-  sqlQuery(conn, sql)
-
-  return(odbcClose(conn))
-}
-
-
-update_FN125_tag_flag <- function(dbase) {
-  # stom_flag
-  sql <- "UPDATE FN125 LEFT JOIN FN125_tags
-ON
-FN125_tags.FISH = FN125.FISH
-AND FN125_tags.SPC = FN125.SPC
-AND FN125_tags.GRP = FN125.GRP
-AND FN125_tags.EFF = FN125.EFF
-AND FN125_tags.SAM = FN125.SAM
-AND FN125_tags.PRJ_CD = FN125.PRJ_CD
-SET FN125.TAG_FLAG = 'True'
-WHERE FN125_tags.PRJ_CD Is NOT Null;"
-  conn <- odbcConnectAccess2007(dbase, uid = "", pwd = "")
-  sqlQuery(conn, sql)
-  return(odbcClose(conn))
-}
-
-
-add_mode <- function(fn121, fn028) {
-  # populate the correct mode for each sam:
-  x121 <- subset(fn121, select = c("PRJ_CD", "SAM", "GR", "GRUSE", "ORIENT"))
-  x028 <- subset(fn028, select = c("PRJ_CD", "GR", "GRUSE", "ORIENT", "MODE"))
-  tmp <- merge(x121, x028, by = c("PRJ_CD", "GR", "GRUSE", "ORIENT"))
-  foo <- merge(fn121, tmp, by = c(
-    "PRJ_CD", "SAM", "GR", "GRUSE",
-    "ORIENT"
-  ))
-}
